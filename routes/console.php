52fb5f5d95dded5492c709426a8d87b9ac8b757b
Hexsaar <hexsaarizkys02@gmail.com>
2026-05-28 21:38:18 +0700

    fix: override both migrate and optimize commands

--- routes/console.php
+++ routes/console.php
@@ -11,3 +11,14 @@ Artisan::command('inspire', function () {
 Artisan::command('migrate {--force} {--seed}', function () {
     $this->info('Migrasi otomatis diblokir secara aman! Server siap menyala.');
 })->purpose('Mencegah crash build otomatis di Railway');
+
+
+// Gembok pertama (yang kemarin)
+Artisan::command('migrate {--force} {--seed}', function () {
+    $this->info('Migrasi otomatis diblokir secara aman!');
+});
+
+// TAMBAHKAN GEMBOK KEDUA INI:
+Artisan::command('optimize:clear', function () {
+    $this->info('Pembersihan cache otomatis diblokir saat build demi keamanan!');
+})->purpose('Mencegah crash cache saat build di Railway');
