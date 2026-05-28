c25af316a4e483ecd3a15cb205b2f6af309e1cd3
Hexsaar <hexsaarizkys02@gmail.com>
2026-05-28 21:19:02 +0700

    feat: override migrate command to bypass railway build crash

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,7 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('migrate', function () {
+    $this->info('Migrasi otomatis diblokir secara aman! Server siap menyala.');
+})->purpose('Mencegah crash build otomatis di Railway');
